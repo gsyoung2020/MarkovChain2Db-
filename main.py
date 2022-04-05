c85eeae2ac8d47eef189a0e54546a5fc3d177660
Grant Young <73722590+gsyoung2020@users.noreply.github.com>
2022-04-05 20:39:29 +0000

    Cleaned up the Generated text in the loop, and now have the ability to commit text to the DB. Next plan it to work on a scheduling system so it will pump to the DB weekly

--- main.py
+++ main.py
@@ -33,6 +33,16 @@ def text_cleaner(text):
   text = re.sub(r'--', ' ', text)
   text = re.sub('[\[].*?[\]]', '', text)
   text = re.sub(r'(\b|\s+\-?|^\-?)(\d+|\d*\.\d+)\b','', text)
+  text = re.sub(r"won\'t", "will not", text)
+  text = re.sub(r"can\'t", "can not", text)
+  text = re.sub(r"n\'t", " not", text)
+  text = re.sub(r"\'re", " are", text)
+  text = re.sub(r"\'s", " is", text)
+  text = re.sub(r"\'d", " would", text)
+  text = re.sub(r"\'ll", " will", text)
+  text = re.sub(r"\'t", " not", text)
+  text = re.sub(r"\'ve", " have", text)
+  text = re.sub(r"\'m", " am", text)
   text = ' '.join(text.split())
   return text
   
@@ -82,15 +92,37 @@ generator_2 = POSifiedText(combined_sents, state_size=3)
 #print("")
 
 #this sentence gen makes tweet size lengts of 140, google say new is 280 gonna keep simple til i do some research 
-for i in range(10):
+for i in range(5):
+ # print("----------")
   loop = i
   captured = generator_2.make_short_sentence(max_chars=140)
-  sql = f"INSERT INTO chatter VALUES ('{captured}',{loop})" 
-  sql = sql.replace("'", "''")
-  print(sql) 
-  cursor.execute(sql)
-  #print(type(captured))
+  #print(f'This is the orignal text:.... {captured}')
+  
+    
+  comma = captured.replace(' ,',',')
+  period = comma.replace(' .', '.')
+  qMark = period.replace(' ?', '?')
+  poss = qMark.replace("'", "''")
+  
+  def decontracted(phrase):
+    # specific
+    phrase = re.sub(r"won\’t", "will not", phrase)
+    phrase = re.sub(r"can\’t", "can not", phrase)
+
+    # general
+    phrase = re.sub(r"n\’t", " not", phrase)
+    phrase = re.sub(r"\’re", " are", phrase)
+    phrase = re.sub(r"\’s", " is", phrase)
+    phrase = re.sub(r"\’d", " would", phrase)
+    phrase = re.sub(r"\’ll", " will", phrase)
+    phrase = re.sub(r"\’t", " not", phrase)
+    phrase = re.sub(r"\’ve", " have", phrase)
+    phrase = re.sub(r"\’m", " am", phrase)
+    return phrase
+
+  output = decontracted(poss)
   sql = "INSERT INTO chatter VALUES (?,?)"
-  cursor.execute(sql, (captured, loop))
-
-
+  cursor.execute(sql, (output, loop))
+  connection.commit()
+  print(decontracted(poss))
+ # print("----------")
